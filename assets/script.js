7ae6e5651f11bac655e109e30ac41440d8ee4763
godot23 <jasonanavon@gmail.com>
2023-07-30 17:21:05 -0400

    created buttons with text in them, the question header, and a timer

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,34 @@
 let quizDiv = document.querySelector("#quiz");
+let h1El = document.querySelector("h1");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
 let questionButton4 = document.querySelector("#answer4");
-let questionButton5 = document.querySelector("#answer5");
-
+let startGame = document.querySelector("#startGameButton");
+let timerTime = document.querySelector("#timerTime");
 
 let timer = 100;
 let currentQuestion = 0;
+
+let questions = [{question: "Which programming language is the worst?", answers: ["R", "Python", "Java", "Javascript"], correctAnswer: "Javascript"},
+{question: "Which programming language is the best", answers: ["R", "Java", "Python", "Javascript"], correctAnswer: "R"}]
+
 renderQuestion();
+
+startGame.textContent = "Click here to start game";
 
 let timerId = setInterval(function(){
     timer -= 1;
     
 }, 1000);
 
-let questions = [{question: "Which programming language is the worst?", answers: ["R", "Python", "Java", "Javascript"], correctAnswer: "Javascript"},
-{question: "Which programming language is the best", answers: ["R", "Java", "Python", "Javascript"], correctAnswer: "R"}]
-
-
-
 function renderQuestion(){
-console.log(questions[0].question);
-console.log(questions[0].answers[0]);
-console.log(questions[0].answers[1]);
-console.log(questions[0].answers[2]);
-console.log(questions[0].answers[3]);
-console.log(questions[currentQuestion].correctAnswer);
+timerTime.textContent = timer;
+h1El.textContent = questions[currentQuestion].question;
+questionButton1.textContent = questions[currentQuestion].answers[0];
+questionButton2.textContent = questions[currentQuestion].answers[1];
+questionButton3.textContent = questions[currentQuestion].answers[2];
+questionButton4.textContent = questions[currentQuestion].answers[3];
 }
 
 quizDiv.addEventListener("click", function(event){
