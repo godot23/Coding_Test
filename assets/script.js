8892e61d9de2f935c7c5d29b2df5b993554fe906
godot23 <jasonanavon@gmail.com>
2023-07-30 19:11:11 -0400

    fixed some things

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 let quizDiv = document.querySelector("#quiz");
-let h1El = document.querySelector("h1");
+let questionText = document.querySelector("#question");
 let questionButton1 = document.querySelector("#answer1");
 let questionButton2 = document.querySelector("#answer2");
 let questionButton3 = document.querySelector("#answer3");
@@ -13,18 +13,27 @@ let currentQuestion = 0;
 let questions = [{question: "Which programming language is the worst?", answers: ["R", "Python", "Java", "Javascript"], correctAnswer: "Javascript"},
 {question: "Which programming language is the best", answers: ["R", "Java", "Python", "Javascript"], correctAnswer: "R"}]
 
-renderQuestion();
+
 
 startGame.textContent = "Click here to start game";
 
-let timerId = setInterval(function(){
-    timer -= 1;
-    
-}, 1000);
+startGame.addEventListener("click", function(event){
+    startGame.remove();
+    timerTime.textContent = timer;
+    timerStart();
+    renderQuestion();
+})
+
+function timerStart(){
+    let timerId = setInterval(function(){
+        timer -= 1;
+        
+    }, 1000);
+}
 
 function renderQuestion(){
-timerTime.textContent = timer;
-h1El.textContent = questions[currentQuestion].question;
+
+questionText.textContent = questions[currentQuestion].question;
 questionButton1.textContent = questions[currentQuestion].answers[0];
 questionButton2.textContent = questions[currentQuestion].answers[1];
 questionButton3.textContent = questions[currentQuestion].answers[2];
